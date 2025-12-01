6bf8b322eb0558c6507652d4710522d3bd29a73a
Mehdi Farshbaf <mehdifarshbaf92@gmail.com>
2025-12-01 16:00:55 +0330

    install packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'localhost',
+      port: 3600,
+      username: 'root',
+      password: '',
+      database: 'nest_ecommerce',
+      entities: [__dirname + '/**/entities/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
