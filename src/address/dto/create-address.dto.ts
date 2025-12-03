eec4d4aa92016950db9a2cc9953d7dc47819e7c8
Mehdi Farshbaf <mehdifarshbaf92@gmail.com>
2025-12-03 12:36:25 +0330

    create address dto

--- src/address/dto/create-address.dto.ts
+++ src/address/dto/create-address.dto.ts
@@ -1,1 +1,27 @@
-export class CreateAddressDto {}
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateAddressDto {
+  @IsString({ message: 'مقدار استان باید رشته باشد.' })
+  @IsNotEmpty({ message: 'مقدار استان الزامی است.' })
+  province: string;
+
+  @IsString({ message: 'شهر باید یک متن باشد.' })
+  @IsNotEmpty({ message: 'شهر الزامی است.' })
+  city: string;
+
+  @IsString({ message: 'کدپستی باید متن باشد.' })
+  @IsNotEmpty({ message: 'کدپستی الزامی است.' })
+  postal_code: string;
+
+  @IsString({ message: 'آدرس باید متن باشد.' })
+  @IsNotEmpty({ message: 'آدرس الزامی است.' })
+  address: string;
+
+  @IsString({ message: 'موبایل گیرنده باید متن باشد.' })
+  @IsNotEmpty({ message: 'موبایل گیرنده الزامی است.' })
+  receiver_mobile: string;
+
+  @IsOptional()
+  @IsString({ message: 'توضیحات باید متنی باشد.' })
+  description?: string;
+}
