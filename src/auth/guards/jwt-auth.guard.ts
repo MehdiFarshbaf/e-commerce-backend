ab158dff1e3b5c10ee4300834c9a695ae6b4bf0b
MehdiFarshbaf <mehdifarshbaf92@gmai.com>
2025-12-29 22:22:57 +0330

    permissions guard

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -2,35 +2,34 @@ import {
   Injectable,
   ExecutionContext,
   UnauthorizedException,
-} from '@nestjs/common'
-import { Reflector } from '@nestjs/core'
-import { AuthGuard } from '@nestjs/passport'
-import { Observable } from 'rxjs'
-import { IS_PUBLIC_KEY } from '../decorators/public.decorator'
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor (private reflector: Reflector) {
-    super()
+  constructor(private reflector: Reflector) {
+    super();
   }
 
   // اجازه رد شدن از گارد رو میده اگر return true کنه
-  canActivate (context: ExecutionContext) {
+  canActivate(context: ExecutionContext) {
     const isPublic = this.reflector.getAllAndOverride(IS_PUBLIC_KEY, [
       context.getClass(), // برای کل کلاس
       context.getHandler(), // برای هندلر های داخل کلاس
-    ])
+    ]);
     if (isPublic) {
-      return true
+      return true;
     }
-    return super.canActivate(context)
+    return super.canActivate(context);
   }
 
-  handleRequest (err: any, user: any, info: any, context: ExecutionContext) {
+  handleRequest(err: any, user: any, info: any, context: ExecutionContext) {
     if (err || !user) {
-      throw err || new UnauthorizedException('توکن شما نا معتبر است.')
+      throw err || new UnauthorizedException('توکن شما نا معتبر است.');
     }
-    console.log('user is : ', user)
-    return user
+    console.log('user is : ', user);
+    return user;
   }
 }
---
+++ src/auth/guards/permissions.guard.ts
@@ -0,0 +1,41 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { AuthService } from '../auth.service';
+import { Reflector } from '@nestjs/core';
+import { PERMISSIONS_KEY } from '../decorators/permissions.decorator';
+
+@Injectable()
+export class PermissionsGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext) {
+    const requiredPermissions = this.reflector.getAllAndOverride<string[]>(
+      PERMISSIONS_KEY,
+      [context.getClass(), context.getHandler()],
+    );
+
+    if (!requiredPermissions) return true;
+
+    // get user data
+    const { user } = context.switchToHttp().getRequest();
+    const userId = user.id;
+
+    const userPermissions = await this.authService.getUserPermissions(userId);
+
+    const hasPermissions = requiredPermissions.every((permission) =>
+      userPermissions.includes(permission),
+    );
+
+    if (!hasPermissions)
+      throw new ForbiddenException('شما مجوز لازم برای این عملیات رو ندارید.');
+
+    return true;
+  }
+}
